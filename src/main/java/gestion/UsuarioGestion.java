7d9b81dd9e44a52dfbd5e7d373c402c764d705ad
kevin <kevin@DESKTOP-BOGOFL7>
2021-08-01 23:42:14 -0600

    edicion del procedimiento almacenado y llamado a paquete orcl oracle

--- src/main/java/gestion/UsuarioGestion.java
+++ src/main/java/gestion/UsuarioGestion.java
@@ -10,12 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java .sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Conexion;
-import model.Persona;
 import model.Usuario;
 import oracle.jdbc.OracleTypes;
 
